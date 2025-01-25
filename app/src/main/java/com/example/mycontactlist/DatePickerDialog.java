78f2be7d8cc593b41513db6f03aaaeef3a90233c
Ajdin Seho <ajdinseho@hotmail.com>
2025-01-24 23:48:59 -0500

    Chapter 4 main project finished

--- app/src/main/java/com/example/mycontactlist/DatePickerDialog.java
+++ app/src/main/java/com/example/mycontactlist/DatePickerDialog.java
@@ -28,8 +28,7 @@ public class DatePickerDialog extends DialogFragment {
 
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         final View view = inflater.inflate(R.layout.select_date, container);
 
         getDialog().setTitle("Select Date");
@@ -39,15 +38,20 @@ public class DatePickerDialog extends DialogFragment {
         cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange( CalendarView calendarView, int year, int month, int day) {
-                selectedDate.set(year,month,day);
+                selectedDate.set(year, month, day);
             }
         });
-        Button cancelButton = view.findViewById(R.id.buttonCancel);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+
+        Button saveButton = view.findViewById(R.id.buttonSelect);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 saveItem(selectedDate);
             }
+        });
+        Button cancelButton = view.findViewById(R.id.buttonCancel);
+        cancelButton.setOnClickListener(v ->{
+            getDialog().dismiss();
         });
         return view;
     }
