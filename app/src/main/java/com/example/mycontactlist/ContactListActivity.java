6d531c2231d4718cdccae2a39e70c440aed98f3f
Ajdin Seho <ajdinseho@hotmail.com>
2025-02-24 18:55:02 -0500

    so far so goodish

--- app/src/main/java/com/example/mycontactlist/ContactListActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -100,17 +100,16 @@ public class ContactListActivity extends AppCompatActivity {
 
     private void initMapButton(){
         ImageButton ibList = findViewById(R.id.imageButtonMap);
-        ibList.setEnabled(false);
-
-    }
-
-    private void initSettingsButton(){
-        ImageButton ibList = findViewById(R.id.imageButtonSettings);
         ibList.setOnClickListener(v -> {
             Intent intent = new Intent(ContactListActivity.this, ContactSettingsActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         });
+    }
+
+    private void initSettingsButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setEnabled(false);
     }
 
     private void initAddContactButton(){
