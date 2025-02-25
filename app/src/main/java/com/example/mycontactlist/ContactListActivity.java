af9f80c9d8c0321cfc24e08f0f3405ceb6c42c43
Ajdin Seho <ajdinseho@hotmail.com>
2025-02-24 19:10:28 -0500

    so far so goodish

--- app/src/main/java/com/example/mycontactlist/ContactListActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -101,15 +101,22 @@ public class ContactListActivity extends AppCompatActivity {
     private void initMapButton(){
         ImageButton ibList = findViewById(R.id.imageButtonMap);
         ibList.setOnClickListener(v -> {
+            Intent intent = new Intent(ContactListActivity.this, ContactMapActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+
+
+
+    }
+
+    private void initSettingsButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(v -> {
             Intent intent = new Intent(ContactListActivity.this, ContactSettingsActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         });
-    }
-
-    private void initSettingsButton(){
-        ImageButton ibList = findViewById(R.id.imageButtonList);
-        ibList.setEnabled(false);
     }
 
     private void initAddContactButton(){
--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -1,14 +1,23 @@
 package com.example.mycontactlist;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
 
@@ -53,4 +62,44 @@ public class ContactMapActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+    private void initGetLocationButton(){
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editAddress = (EditText) findViewById(R.id.editAddress);
+                EditText editCity = (EditText) findViewById(R.id.editCity);
+                EditText editState = (EditText) findViewById(R.id.editState);
+                EditText editZipCode = (EditText) findViewById(R.id.editZipCode);
+
+                String address = editAddress.getText().toString() + ", " +
+                                    editCity.getText().toString() + ", " +
+                                    editState.getText().toString() + ", " +
+                                    editZipCode.getText().toString();
+
+                List<Address> addresses = null;
+                Geocoder geo = new Geocoder(ContactMapActivity.this);
+                try{
+                    addresses = geo.getFromLocationName(address, 1);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+
+                txtLatitude.setText(String.valueOf(addresses.get(0).getLatitude()));
+                txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
+
+            }
+        });
+
+
+
+    }
+
+
+
+
 }
